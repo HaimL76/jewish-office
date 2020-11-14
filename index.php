51cec08fce569f4392127efed528ff311483a0f2
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-11-14 19:11:29 +0200

    3

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<?php
-	//include 'connection.php';
-	require_once 'connection.php';
-
-    $conn = openConnection();
-    
-    if ($conn == null)// || $conn.empty())
-        die("Connection object is invalid");
-
-    //echo json_encode($conn);
-
-//	$sql = "SELECT * FROM tabel_name WHERE userid='" . $user_id . "';";
-//	$result_get_user = $conn->query($sql);
-
-//	while ($row = $result_get_user->fetch_assoc()) {
-//		$data_get_user[] = $row;
-//	}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,6 +6,7 @@
     <title>tora</title>
 </head>
 <body>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, deserunt non quos, illo rem tenetur fuga placeat iusto tempore reiciendis, maxime alias libero molestiae exercitationem? Sint quo autem veniam vitae!
+    <a href=users-listing.html>לרשימת משתמשים לחץ כאן</a><br>
+    <a href=books-listing.html>לרשימת ספרים לחץ כאן</a>
 </body>
 </html>
