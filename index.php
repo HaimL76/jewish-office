b5d9e8aa409094d227f1605a85248697a4b0f92e
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-11-16 23:31:48 +0200

    5

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tora</title>
+    <title>tora</title>1
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <a href=users-listing.html>לרשימת משתמשים לחץ כאן</a><br>
