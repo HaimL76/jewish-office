449db4db7ac95c8b331609455bb495848563a960
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-11-18 01:44:47 +0200

    7

--- post_book.php
+++ post_book.php
@@ -3,31 +3,38 @@
 
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') 
         die ("request is not a post method");
+
+    if (!isset($_POST)) 
+        die ("no post parameters found");
     
     header("Content-Type:application/json");
 
     $sys_id = 'sys_id';
     $name = 'name';
 
-    $sysid = $_POST[$sys_id];
-    $bname = $_POST[$name];
+    foreach ($_POST as $key => $value){
+        error_log("{$key} = {$value}\r\n");
+      }
 
-    if (isset ($sysid) && $sysid > 0 && isset($bname) && $bname != '') {
-        $conn = openConnection();
-        
-        if ($conn == null)// || $conn.empty())
-            die("Connection object is invalid");
-
-        //echo json_encode($conn);
-
-        $sql = "insert into ta_user (sys_id, name) values (" + $sysid + ", '" + $bname + "');";
-        
-        error_log($sql . PHP_EOL);
-
-        $result = $conn->query($sql);
-
-        echo $result;
-
-        $conn->close();
-    }
+      $sysid = $_POST[$sys_id];
+      $bname = $_POST[$name];
+  
+      if (isset ($sysid) && $sysid > 0 && isset($bname) && $bname != '') {
+          $conn = openConnection();
+          
+          if ($conn == null)// || $conn.empty())
+              die("Connection object is invalid");
+  
+          //echo json_encode($conn);
+  
+          $sql = "insert into ta_user (sys_id, name) values (" + $sysid + ", '" + $bname + "');";
+          
+          error_log($sql . PHP_EOL);
+  
+          $result = $conn->query($sql);
+  
+          echo $result;
+  
+          $conn->close();
+      }
 ?>
