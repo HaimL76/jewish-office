03df2a0e754a5f96cdff224d9204a864bce1b765
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-11-20 09:51:51 +0200

    7

--- post_book.php
+++ post_book.php
@@ -6,18 +6,23 @@
 
     if (!isset($_POST)) 
         die ("no post parameters found");
+
+    //var_dump($_POST);
     
     header("Content-Type:application/json");
 
     $sys_id = 'sys_id';
     $name = 'name';
 
-    foreach ($_POST as $key => $value){
-        error_log("{$key} = {$value}\r\n");
-      }
+    //foreach ($_POST as $key => $value){
+        //error_log("{$key} = {$value}\r\n");
+      //}
 
-      $sysid = $_POST[$sys_id];
-      $bname = $_POST[$name];
+      $sysid = $_POST["sys_id"];//$sys_id];
+      $bname = $_POST["name"];//$name];
+
+      //echo $sysid;
+      //echo $name;
   
       if (isset ($sysid) && $sysid > 0 && isset($bname) && $bname != '') {
           $conn = openConnection();
@@ -27,13 +32,17 @@
   
           //echo json_encode($conn);
   
-          $sql = "insert into ta_user (sys_id, name) values (" + $sysid + ", '" + $bname + "');";
+          $sql = "insert into ta_book (sys_id, name) values ({$sysid}, '{$bname}')";
           
-          error_log($sql . PHP_EOL);
+          echo $sql;
+
+          //error_log($sql . PHP_EOL);
   
           $result = $conn->query($sql);
   
           echo $result;
+          
+          echo "res: {$result} ult";
   
           $conn->close();
       }
